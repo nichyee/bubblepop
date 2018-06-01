ddad46450ba735a624bc989512c9aa30afa07e1b
nichyee <30689456+nichyee@users.noreply.github.com>
2018-06-01 17:13:33 +1000

    Add files via upload

---
+++ bubblepop/BubbleButton.swift
@@ -0,0 +1,27 @@
+//
+//  BubbleButton.swift
+//  bubblepop
+//
+//  Created by Nicholas Yee on 30/4/18.
+//  Copyright © 2018 Nicholas Yee. All rights reserved.
+//
+
+import UIKit
+
+
+class BubbleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var name = ""
+    var points = 0
+    
+    
+
+}
